ccdb37883fffd755681fb648290adf6fb56b94fd
S0ngRu1 <1922909737@qq.com>
2024-10-13 19:49:48 +0800

    feat:add strcount.py

---
+++ strcount.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time : 2024/10/13 18:39
+# @Author : CSR
+# @File : strcount.py.py
+message = "sadfasdffgfhtujru etwefdsjhr fddsszzbnn erterter4 cbx"
+char_count = {}
+for i in range(len(message)):
+    char_count.setdefault(message[i], 0)
+    char_count[message[i]] += 1
+
+print(char_count)
